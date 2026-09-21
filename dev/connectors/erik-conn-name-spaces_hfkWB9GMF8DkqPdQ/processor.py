12efd6be7e4b9799c8221e4e607f2050339b700b
Erik Riis <125295923+eriis1@users.noreply.github.com>
2026-09-21 13:51:49 -0700

    Update processor.py

--- dev/connectors/erik-conn-name-spaces_hfkWB9GMF8DkqPdQ/processor.py
+++ dev/connectors/erik-conn-name-spaces_hfkWB9GMF8DkqPdQ/processor.py
@@ -6,6 +6,8 @@ def execute(**kwargs) -> UploadFileParams:
     filename = "changeme.txt"
     body = bytes("Hello, World!", "utf-8")
 
+    print("TESTING REPO SYNC")
+
     new_file_param = FileParam(filename=filename, body=body)
 
     return UploadFileParams(files=[new_file_param])
