c688f544dc3581b0e6ab3949e110278d739f4588
eriis1 <erik@ganymede.bio>
2026-09-25 15:37:41 +0000

    Sync flows in environment dev from Ganymede

--- dev/flows/erik-test/CSV_Read/function.py
+++ dev/flows/erik-test/CSV_Read/function.py
@@ -23,7 +23,7 @@ def execute(csv_file: dict[str, BytesIO], ganymede_context: GanymedeContext) -> 
         Object containing data to store in data lake and/or file storage.
     """
 
-    print("Hello!")
+    print("Hello")
 
     results_dict = dict()
 
