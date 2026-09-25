c01ddfdf52cec0b2b4d5a3f5dbf81132dd1590b9
Erik Riis <125295923+eriis1@users.noreply.github.com>
2026-09-25 09:08:17 -0700

    Update function.py

--- dev/flows/erik-test/CSV_Read/function.py
+++ dev/flows/erik-test/CSV_Read/function.py
@@ -23,7 +23,7 @@ def execute(csv_file: dict[str, BytesIO], ganymede_context: GanymedeContext) -> 
         Object containing data to store in data lake and/or file storage.
     """
 
-    print("Hello2")
+    print("Hello3")
 
     results_dict = dict()
 
